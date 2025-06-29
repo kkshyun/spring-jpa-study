f56b005c0f2617b1803f9c37af2596f75561d059
sehyunnnn <kkshyun56@gmail.com>
2025-06-30 02:31:59 +0900

    feat : section 5(1)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,38 +1,37 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
-@Entity
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity // JPA가 관리하는 객체
+@Table(name="MBR") // 테이블 명을 바꾸고 싶을 때
 public class Member {
+//    @Id
+//    private Long id;
+//    @Column(unique = true, length = 10)
+//    private String name;
     @Id
     private Long id;
-    private String name;
+    @Column(name = "name", nullable = false)
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP) // 과거 버전
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate createDate2; // 최신 버전 - 알아서 매핑 해줌
+    private LocalDateTime testLocalDateTime;
+    @Lob // 큰 콘텐츠를 넣고 싶으면
+    private String description;
+
 
     public Member() {
 
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
